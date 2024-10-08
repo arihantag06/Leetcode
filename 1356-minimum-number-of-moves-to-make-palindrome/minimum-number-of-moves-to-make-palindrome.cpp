0668aa48f4b5f2edfbfde32b076aab9c9c8c9d18
Arihant Agarwal <119732049+Apollofy@users.noreply.github.com>
2024-10-08 23:37:49 +0530

    Time: 11 ms (77.81%) | Memory: 8.3 MB (46.20%) - LeetSync

---
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minMovesToMakePalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+        int moves = 0;
+
+        while (left < right) {
+            if (s[left] == s[right]) {
+                left++;
+                right--;
+            } else {
+                
+                int k = right;
+                while (k > left && s[k] != s[left]) {
+                    k--;
+                }
+
+
+                if (k == left) {
+                    
+                    swap(s[left], s[left + 1]);
+                    moves++;
+                } else {
+                    
+                    while (k < right) {
+                        swap(s[k], s[k + 1]);
+                        k++;
+                        moves++;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return moves;
+    }
+};
