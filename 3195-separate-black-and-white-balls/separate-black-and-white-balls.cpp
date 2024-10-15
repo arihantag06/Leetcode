2863a4f12a1d1b8e122e0e2da26aec8d5ee79fbf
Arihant Agarwal <119732049+Apollofy@users.noreply.github.com>
2024-10-15 18:41:17 +0530

    Time: 40 ms (16.21%) | Memory: 15.7 MB (45.68%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        long long ans=0;
-        long long count=0;
+        long long ans = 0;
+        long long count = 0;
         int n = s.length();
-       vector<long long>zeros(n,0);
-       for(int i=n-1;i>=0;i--){
-        if(s[i]=='0'){
-        count++;
+        for (int i = n - 1; i >= 0; i--) {
+            if(s[i]=='0'){
+                count++;
+            }
+            else if(s[i]=='1'){
+                ans+=count;
+            }
         }
-        zeros[i] = max(zeros[i],count);
-       }
-
-       for(int i=0;i<n;i++){
-        if(s[i]=='1'){
-            ans+=zeros[i];
-        }
-       }
-       return ans;
+        return ans;
     }
 };
