62e85a11917924631a177dd3845750349d2db1c9
Arihant Agarwal <119732049+Apollofy@users.noreply.github.com>
2024-10-24 01:09:59 +0530

    Time: 19 ms (97.91%) | Memory: 21.5 MB (95.59%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    
     string longestWord(vector<string>& words) {
-        int n=words.size();
-        sort(words.begin(), words.end(), [](const string& a, const string& b) {
-            return a.size() < b.size();
-        });
-        set<string>st;
-        for(string s:words){
-            st.insert(s);
-        }
-
-        vector<string>v;
-        for(int i=0;i<n;i++){
-            string word = words[i];
-            bool f =true;
-            int sz = word.length();
-            string temp;
-            for(int j=0;j<sz;j++){
-                temp+=word[j];
-                if(st.find(temp)==st.end()){
-                    f=false;
-                    break;
+        
+        sort(words.begin(), words.end());
+        
+        set<string> built; 
+        string res;
+        
+       
+        for (const string& word : words) {
+            int len = word.size();
+            if (len == 1 || built.count(word.substr(0, len - 1))) {
+                built.insert(word);  
+                if (word.size() > res.size()) {
+                    res = word;
                 }
             }
-            if(f){
-                v.push_back(word);
-            }
         }
-        sort(v.begin(), v.end(), [](const string& a, const string& b) {
-            return a.size() < b.size();
-        });
-        if(v.empty())return "";
-        int maxLen = v[v.size()-1].length();
-        string res = v[v.size()-1];
-        for(int i=v.size()-2;i>=0;i--){
-            int currLen = v[i].length();
-            if(currLen==maxLen){
-                if(v[i]<res) res = v[i];
-            }
-        }
+        
         return res;
     }
 };
