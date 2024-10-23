3dbdd77e132059aa8391253fc3e051d7437d7c98
Arihant Agarwal <119732049+Apollofy@users.noreply.github.com>
2024-10-23 17:36:34 +0530

    Time: 2 ms (47.50%) | Memory: 10.8 MB (14.55%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int dp[101];
     int solve(int idx , string s){
-        if(s[idx]=='0')return 0;
         if(idx==s.size())return 1;
         if(dp[idx]!=-1)return dp[idx];
-        int two = -1;
-        if(idx+1!=s.size() || s[idx]=='0'){
-            two = ((s[idx]-'0')*10) + (s[idx+1]-'0');
+        
+        int res = 0;
+        if(s[idx]!='0'){
+            res+=solve(idx+1,s);
         }
-
-        if(two!=-1 && two<=26){
-            return dp[idx]= solve(idx+2,s) + solve(idx+1,s);
+        if(idx+1<s.size() && (s[idx]=='1' || s[idx]=='2' && s[idx+1]<='6')){
+            res+=solve(idx+2,s);
         }
-        return dp[idx]= solve(idx+1,s);
+        return dp[idx] = res;
+       
     }
     int numDecodings(string s) {
         if(s[0]=='0')return 0;
