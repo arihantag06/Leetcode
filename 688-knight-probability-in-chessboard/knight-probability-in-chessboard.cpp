b7eccb53b51fb8fede24939fcc011d3651ea2b08
Arihant Agarwal <119732049+Apollofy@users.noreply.github.com>
2024-10-24 15:54:28 +0530

    Time: 2 ms (96.65%) | Memory: 8 MB (96.45%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    const int mod = 1e9+7;
-    vector<vector<vector<double>>> memo;
-
-    double solve(int row, int col, int n, int k) {
-        if (row < 0 || row >= n || col < 0 || col >= n) return 0;
-        if (k == 0) return 1;
-        if (memo[row][col][k] != -1) return memo[row][col][k];
-
-        vector<pair<int, int>> moves = {{-2, -1}, {-2, +1}, {-1, -2}, {-1, +2}, 
-                                        {+1, -2}, {+1, +2}, {+2, -1}, {+2, +1}};
-        double prob = 0;
-        for (auto move : moves) {
-            int newRow = row + move.first;
-            int newCol = col + move.second;
-            prob += solve(newRow, newCol, n, k - 1) / 8.0;
-        }
-
-        return memo[row][col][k] = prob;
+    double dp[26][26][101];
+    double solve(int row, int col, int n, int k){
+        if(row<0 || row>=n || col<0 || col>=n)return 0;
+        if(k==0)return 1;
+        if(dp[row][col][k]!=-1)return dp[row][col][k];
+        
+        double sum=0;
+        sum+= solve(row-2,col-1,n,k-1)/8.0;
+        sum+= solve(row-2,col+1,n,k-1)/8.0;
+        sum+= solve(row-1,col-2,n,k-1)/8.0;
+        sum+= solve(row-1,col+2,n,k-1)/8.0;
+        sum+= solve(row+1,col-2,n,k-1)/8.0;
+        sum+= solve(row+1,col+2,n,k-1)/8.0;
+        sum+= solve(row+2,col-1,n,k-1)/8.0;
+        sum+= solve(row+2,col+1,n,k-1)/8.0;
+        return dp[row][col][k] = sum;
     }
-
     double knightProbability(int n, int k, int row, int column) {
-        memo = vector<vector<vector<double>>>(n, vector<vector<double>>(n, vector<double>(k + 1, -1)));
-        return solve(row, column, n, k);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int l=0;l<=k;l++){
+                    dp[i][j][l]=-1.0;
+                }
+            }
+        }
+        return solve(row,column,n,k);
     }
 };
