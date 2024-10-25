13ef60f762e99a636828dbd1294c53eb2593c449
Arihant Agarwal <119732049+Apollofy@users.noreply.github.com>
2024-10-26 00:32:22 +0530

    Time: 3 ms (46.40%) | Memory: 12.3 MB (5.91%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int papers = citations.size();
-        int N = papers;
-        sort(begin(citations),end(citations));
-        int maxi = INT_MIN;
-        while(papers){
-            int idx = lower_bound(begin(citations),end(citations),papers)-begin(citations);
-            int diff = N-idx;
-            if(diff>=papers)maxi = max(maxi,papers);
-            papers--;
+        // int papers = citations.size();
+        // int N = papers;
+        // sort(begin(citations),end(citations));
+        // int maxi = INT_MIN;
+        // while(papers){
+        //     int idx = lower_bound(begin(citations),end(citations),papers)-begin(citations);
+        //     int diff = N-idx;
+        //     if(diff>=papers)maxi = max(maxi,papers);
+        //     papers--;
+        // }
+        // return maxi==INT_MIN?0:maxi;
+        sort(rbegin(citations),rend(citations));
+        int N = citations.size();
+        int i=0;
+        for(i;i<N;i++){
+            if(i+1>citations[i])return i;
         }
-        return maxi==INT_MIN?0:maxi;
+        return i;
     }
 };
