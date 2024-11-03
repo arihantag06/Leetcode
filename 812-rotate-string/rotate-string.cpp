d734856faf3671498e2e009d3dd4d6fcf8db2f97
Arihant Agarwal <119732049+Apollofy@users.noreply.github.com>
2024-11-03 10:54:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (55.81%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+       if (s.size() != goal.size()) {
+        return false;
+    }
+    string concatenated = s + s;
+    return concatenated.find(goal) != string::npos;
+    }
+};
