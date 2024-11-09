d146d9ab43440e506ef97ed2736a5b57963b9edd
Arihant Agarwal <119732049+Apollofy@users.noreply.github.com>
2024-11-09 23:10:36 +0530

    Time: 1762 ms (5.36%) | Memory: 8.5 MB (52.80%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long result = x;
-        long long rem = n-1;
-        long long pos=1;
-        while(rem){
-            if(!(x&pos)){
-                result|=(rem&1)*pos;
-                rem>>=1;
-            }
-            pos<<=1;
+        long long ans = x;
+        for(int i=1;i<n;i++){
+            ans = (ans+1)|x;
         }
-        return result;
+        return ans;
     }
 };
