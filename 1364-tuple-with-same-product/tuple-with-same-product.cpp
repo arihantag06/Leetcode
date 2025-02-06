0ae64a03d919d2fc0fba925f58c977da54bc570f
Arihant <119732049+Apollofy@users.noreply.github.com>
2025-02-06 16:16:56 +0530

    Time: 311 ms (64.89%) | Memory: 85 MB (45.45%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         int n = nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
