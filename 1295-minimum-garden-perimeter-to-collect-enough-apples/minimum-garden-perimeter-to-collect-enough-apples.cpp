446627ed5194150dca807caf3f0f4716694eed46
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-03 17:37:28 +0530

    Time: 7 ms (33.52%) | Memory: 8.1 MB (83.80%) - LeetSync

--- 1295-minimum-garden-perimeter-to-collect-enough-apples/minimum-garden-perimeter-to-collect-enough-apples.cpp
+++ 1295-minimum-garden-perimeter-to-collect-enough-apples/minimum-garden-perimeter-to-collect-enough-apples.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    
-    long long val(long long x) {
-        return 2 * x * (x + 1) * (2 * x + 1);
-    }
-
     long long minimumPerimeter(long long neededApples) {
-        long long low = 0, high = 1;
-        while (val(high) < neededApples) high <<= 1;
-        long long ans = high;
-        while (low <= high) {
-            long long mid = low + (high - low) / 2;
-            if (val(mid) >= neededApples) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+        long long n =0,sum=0;
+        while(sum<neededApples){
+            sum+=12*n*n;
+            n++;
         }
-        return 8 * ans;
+        return (n-1)*8;
     }
 };
