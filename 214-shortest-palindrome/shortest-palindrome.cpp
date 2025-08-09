ac713ba46cc4a1622c63d521284ee8698f77dc76
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-10 00:35:56 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (75.38%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> computeLPS(string p){
+        int n = p.size();
+        vector<int> lps(n,0);
+        int prev = 0, i = 1;
+        while(i<n){
+            if(p[i]==p[prev]){
+                lps[i++] = ++prev;
+            } else if(prev==0){
+                i++;
+            } else {
+                prev = lps[prev-1];
+            }
+        }
+        return lps;
+    }
+    int kmp(string text,string pattern){
+        vector<int> lps = computeLPS(pattern);
+        int i = 0, j = 0;
+        while(i < (int)text.size()){
+            if(text[i] == pattern[j]) { i++; j++; }
+            else if(j == 0) { i++; }
+            else { j = lps[j-1]; }
+        }
+        return j; 
+    }
+
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());        
+        int L = kmp(rev, s);     
+        return rev.substr(0, s.size() - L) + s; 
+    }
+};
