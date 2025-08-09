6683793618de8b6fba55bb52b05ce948382c885c
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-09 19:14:04 +0530

    Time: 55 ms (53.27%) | Memory: 11.5 MB (48.80%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int l, int r, int k, string &s, vector<int>& hash) {
+    int solve(int l, int r, int k, string &s) {
         if (l > r) return 0;
 
         vector<int> freq(26, 0);
@@ -10,7 +10,7 @@ public:
 
         for (int i = l; i <= r; i++) {
             if (freq[s[i] - 'a'] < k) {
-                return max(solve(l, i - 1, k, s, hash), solve(i + 1, r, k, s, hash));
+                return max(solve(l, i - 1, k, s), solve(i + 1, r, k, s));
             }
         }
 
@@ -20,12 +20,6 @@ public:
     int longestSubstring(string s, int k) {
         int n = s.size();
         if (n < k) return 0;
-
-        vector<int> hash(26, 0);
-        // for (int i = 0; i < n; i++) {
-        //     hash[s[i] - 'a']++;
-        // }
-
-        return solve(0, n - 1, k, s, hash);
+        return solve(0, n - 1, k, s);
     }
 };
