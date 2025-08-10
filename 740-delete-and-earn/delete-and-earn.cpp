c89eb1380ba85a0abf93606d77ec61ee0b9557b5
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-10 19:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (97.60%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
         
-        int max_num = 0;
+        int maxi = 0;
         for(int i=0; i < nums.size(); i++){
-            max_num = max(max_num, nums[i]);
+            maxi = max(maxi, nums[i]);
         }
-        vector<int> dp(max_num + 4, 0);
+        vector<int> dp(maxi + 4, 0);
         for(int i=0; i < nums.size(); i++){
             dp[nums[i]] += nums[i];
         }
-        for(int i=max_num; i > -1; i--){
+        for(int i=maxi; i > -1; i--){
             dp[i] += max(dp[i + 2], dp[i + 3]);
         }
         return max({dp[0], dp[1]});
