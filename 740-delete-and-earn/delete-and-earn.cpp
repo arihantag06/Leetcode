d1e816f7cbd26cd34fe17a7eac2a4df090308439
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-10 19:50:55 +0530

    Time: 3 ms (62.80%) | Memory: 15.7 MB (84.36%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int dp[20005][2];
-    int solve(int idx, vector<int>& v, int prevTaken, map<int,int>& mp) {
-        if (idx >= (int)v.size()) return 0;
-        if(dp[idx][prevTaken]!=-1) return dp[idx][prevTaken];
-        int notpick = solve(idx + 1, v,0, mp);
-        bool f = (prevTaken && idx > 0 && v[idx] == v[idx - 1] + 1);
-        int pick = f ? INT_MIN : v[idx] * mp[v[idx]] + solve(idx + 1, v,1, mp);
-        return dp[idx][prevTaken] = max(notpick, pick);
-    }
-
     int deleteAndEarn(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        map<int,int> mp;
-        vector<int> v;
-        v.reserve(nums.size());
-        for (int x : nums) {
-            if (mp.find(x) == mp.end()) v.push_back(x);
-            mp[x]++;
+        
+        int max_num = 0;
+        for(int i=0; i < nums.size(); i++){
+            max_num = max(max_num, nums[i]);
         }
-        sort(v.begin(), v.end());
-
-        return solve(0, v, 0, mp);
+        vector<int> dp(max_num + 4, 0);
+        for(int i=0; i < nums.size(); i++){
+            dp[nums[i]] += nums[i];
+        }
+        for(int i=max_num; i > -1; i--){
+            dp[i] += max(dp[i + 2], dp[i + 3]);
+        }
+        return max({dp[0], dp[1]});
     }
 };
