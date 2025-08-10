c549fa90740128bdf97ee0cb9d38cca114bdb45c
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-10 23:24:52 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (62.83%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool reorderedPowerOf2(int n) {
         string t = to_string(n);
+        int p = log(1e9)/log(2);
+        cout<<p<<endl;
         sort(t.begin(),t.end());
         for(int i=0;i<30;i++){
             string s = to_string(1<<i);
