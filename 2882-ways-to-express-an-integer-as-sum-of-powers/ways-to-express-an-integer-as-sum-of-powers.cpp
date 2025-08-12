60e42bf993c4faac6d5f1adc25fe1f157fdb4db5
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-13 03:57:11 +0530

    Time: 174 ms (55.34%) | Memory: 9.7 MB (93.46%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -2,31 +2,18 @@ class Solution {
 public:
     int dp[301][301];
     const int mod = 1e9+7;
-    int f(int ind, vector<int>&a, int target){
-        if(target==0)return 1;
-        if(ind<0 || target<0)return 0;
-        if(dp[ind][target]!=-1) return dp[ind][target]%mod;
-        int take = f(ind-1,a,target-a[ind]);
-        int nottake = f(ind-1,a,target);
-        return dp[ind][target]=(take+nottake)%mod;
-    }
-    int pow(int num, int p){
-        int prod = 1;
-        while(p--){
-            prod = prod*num;
-        }
-        return prod;
+    int f(int n , int num , int x){
+        if(n==0)return 1;
+        if(n<0)return 0;
+        int p = pow(num,x);
+        if(p>n)return 0;
+        if(dp[n][num]!=-1) return dp[n][num]%mod;
+        int take = f(n-p,num+1,x);
+        int nottake = f(n,num+1,x);
+        return dp[n][num]=(take+nottake)%mod;
     }
     int numberOfWays(int n, int x) {
         memset(dp,-1,sizeof(dp));
-        vector<int>a;
-        for(int i=1;i<=300;i++){
-            int val = pow(i,x);
-            if(val>n)break;
-            a.push_back(val);
-        }
-        int sz = a.size();
-        
-        return f(sz-1,a,n);
+        return f(n,1,x);
     }
 };
