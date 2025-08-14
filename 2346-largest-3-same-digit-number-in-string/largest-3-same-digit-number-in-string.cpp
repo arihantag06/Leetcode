e917b94d44028224f02d456662753f1eba2c4cb4
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-14 21:31:45 +0530

    Time: 4 ms (5.98%) | Memory: 9.4 MB (12.51%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,11 +1,27 @@
+// class Solution {
+// public:
+//     string largestGoodInteger(string num) {
+//         char best = '\0';
+//         int n = num.size();
+//         for (int i = 0; i<n-2; ++i)
+//             if (num[i] == num[i+1] && num[i] == num[i+2] && num[i] > best)
+//                 best = num[i];
+//         return best ? string(3, best) : "";
+//     }
+// };
+
+
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        char best = 0;
-        int n = num.size();
-        for (int i = 0; i<n-2; ++i)
-            if (num[i] == num[i+1] && num[i] == num[i+2] && num[i] > best)
-                best = num[i];
-        return best ? string(3, best) : "";
+        unordered_set<string> mp = {"999","888","777","666","555","444","333","222","111","000"};
+        string ans = "";
+        for (int i = 0; i + 2 < (int)num.size(); i++) {
+            string t = num.substr(i, 3);
+            if (mp.count(t) && t > ans)
+                ans = t;
+        }
+        return ans;
     }
 };
+
