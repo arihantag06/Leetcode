fb948b14a39132f077712bed5cfaae7de092cee3
Arihant <119732049+arihantag06@users.noreply.github.com>
2025-08-14 17:41:31 +0530

    Time: 23 ms (67.02%) | Memory: 136.3 MB (65.29%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -6,9 +6,9 @@ public:
         for(int i=n-1;i>=0;--i){
             int take1=a[i]-dp[i+1];
             int take2=INT_MIN;
-            if(i+2<=n) take2=a[i]+a[i+1]-dp[i+2];
+            if(i+1<n) take2=a[i]+a[i+1]-dp[i+2];
             int take3=INT_MIN;
-            if(i+3<=n) take3=a[i]+a[i+1]+a[i+2]-dp[i+3];
+            if(i+2<n) take3=a[i]+a[i+1]+a[i+2]-dp[i+3];
             dp[i]=max({take1, take2, take3});
         }
         if(dp[0]>0) return "Alice";
